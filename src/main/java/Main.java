461c0aa322b207b1de2da75931f2f8279c7dce90
Vidied <davidepan2001@gmail.com>
2026-06-16 16:29:34 +0200

    save e findbyid evento

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        
+
     }
 }
---
+++ src/main/java/dao/EventoDao.java
@@ -0,0 +1,37 @@
+package dao;
+
+import Entities.Evento;
+import exceptions.NotFoundException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class EventoDao {
+
+    private final EntityManager em;
+
+
+    public EventoDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Evento evento) {
+        EntityTransaction transaction = em.getTransaction();
+
+        transaction.begin();
+
+        em.persist(evento);
+
+        transaction.commit();
+
+        System.out.println("L'evento: " + evento + "è stato salvato nel DB");
+    }
+
+    public Evento findById(long id) {
+
+        Evento fromDB = em.find(Evento.class, id);
+        if(fromDB == null) throw new NotFoundException(id);
+        return fromDB;
+    }
+
+
+}
