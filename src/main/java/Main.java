9db268f367c6d8424eb4044c6ffff6c2b842e0b9
Vidied <davidepan2001@gmail.com>
2026-06-16 16:51:54 +0200

    esercizio completato

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import Entities.EventType;
 import Entities.Evento;
 import dao.EventoDao;
+import exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -20,7 +21,25 @@ public class Main {
         Evento compleanno = new Evento("Compleanno Giangiorgio", LocalDate.of(2000,10,10), "Compleanno del grandissimo Giangiorgio", EventType.PRIVATO, 20);
         Evento concerto = new Evento("Concerto boo", LocalDate.of(3000, 10,1), "Concerto misterioso nell'anno 3000", EventType.PUBBLICO, 100000);
 
-        
+        eventoDao.save(saggio);
+        eventoDao.save(compleanno);
+        eventoDao.save(concerto);
+
+        try {
+            Evento found = eventoDao.findById(1);
+            Evento foundErrore = eventoDao.findById(4);
+            System.out.println(found);
+            System.out.println(foundErrore);
+        } catch (NotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            eventoDao.findByIdAndDelete(2);
+
+        } catch (NotFoundException e) {
+            System.err.println(e.getMessage());
+        }
 
     }
 }
