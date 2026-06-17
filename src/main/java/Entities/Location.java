6472df874b1f24868af191104042d952adbdb8b3
Vidied <davidepan2001@gmail.com>
2026-06-17 15:49:52 +0200

    location basic basic

--- src/main/java/Entities/Location.java
+++ src/main/java/Entities/Location.java
@@ -1,12 +1,45 @@
 package Entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+
+import java.util.UUID;
 
 @Entity
 public class Location {
 
     @Id
     @GeneratedValue
+    @Column(name = "location_id")
+    private UUID id;
+
+    private String nome;
+
+    private String citta;
+
+    public Location(){};
+
+    public Location(String nome, String citta){
+        this.nome = nome;
+        this.citta = citta;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", citta='" + citta + '\'' +
+                '}';
+    }
 }
